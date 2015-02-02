fab6e4f4a013992461fa18ab84b67f99ac2f5354
Vincent LAURENT <vince.laurent@gmail.com>
2015-02-02 15:11:51 +0100

    Script compatible tout awk

--- bin/recap.sh
+++ bin/recap.sh
@@ -7,4 +7,4 @@ if test "$1"; then
 fi
 
 echo "Projet;Tâche;Temps (en heure)"
-cat $FILE_TEMPS | grep -v "Projet;Description;" | cut -d ";" -f 2,4,5 | sed 's/,/./g' | sort | awk -F ";" '{ print $1 ";" $3 ";" $2 }' | awk -F ";" '{ projets[$1][$2] += $3; projets[$1]["ZZZZZ"] += $3; }END{for (projet in projets) { for (task in projets[projet]) {print projet ";" task ";" projets[projet][task] }}}' | sort | sed 's/;ZZZZZ;/;TOTAL;/' | sed 's/\./,/g'
+cat $FILE_TEMPS | grep -v "Projet;Description;" | cut -d ";" -f 2,4,5 | sed 's/,/./g' | sort | awk -F ";" '{ print $1 ";" $3 ";" $2 }' | awk -F ";" '{ projets[$1 ";" $2] += $3; projets[$1 ";ZZZZZ"] += $3; }END{for (projet in projets) {print projet ";" projets[projet] }}' | sort | sed 's/;ZZZZZ;/;TOTAL;/' | sed 's/\./,/g'
