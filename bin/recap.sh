8a3ace83ac431f294c6ea263632b1483f267b287
Vincent LAURENT <vince.laurent@gmail.com>
2015-10-01 17:22:43 +0200

    Ajout du nombre de jour dans le script de récap

--- bin/recap.sh
+++ bin/recap.sh
@@ -6,5 +6,5 @@ if test "$1"; then
     FILE_TEMPS=$1
 fi
 
-echo "Projet;Tâche;Temps (en heure)"
-cat $FILE_TEMPS | grep -v "Projet;Description;" | cut -d ";" -f 2,4,5 | sed 's/,/./g' | sort | awk -F ";" '{ print $1 ";" $3 ";" $2 }' | awk -F ";" '{ projets[$1 ";" $2] += $3; projets[$1 ";ZZZZZ"] += $3; }END{for (projet in projets) {print projet ";" projets[projet] }}' | sort | sed 's/;ZZZZZ;/;TOTAL;/' | sed 's/\./,/g'
+echo "Projet;Tâche;Temps (en heure);Temps (en jours)"
+cat $FILE_TEMPS | grep -v "Projet;Description;" | cut -d ";" -f 2,4,5 | sed 's/,/./g' | sort | awk -F ";" '{ print $1 ";" $3 ";" $2 }' | awk -F ";" '{ projets[$1 ";" $2] += $3; projets[$1 ";ZZZZZ"] += $3; }END{for (projet in projets) { printf("%s;%s;%0.1f\n", projet, projets[projet], projets[projet]/7) }}' | sort | sed 's/;ZZZZZ;/;TOTAL;/' | sed 's/\./,/g'
