c089629158d17709a6873683c9364b62303937af
Tangui Morlier <tangui@tangui.eu.org>
2022-02-07 13:03:39 +0100

    Vérification de la colonne client pour un csv passé en paramètre

---
+++ bin/check_clients.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ls temps/20* | grep -v ':' | sed 's/.csv//'  | sort | uniq -c | sort -n | sed 's/^ *//' | sed 's/ /;/' | sed 's/$/;/'  > /tmp/projets.list
+cat $1 | grep ^2 | awk -F ';' '{print $2}' | while read client; do grep ";$client;" /tmp/projets.list || echo "== nouveau $client ==" ; done
