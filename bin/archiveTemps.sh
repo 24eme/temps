f957edbd17c6dd3b973a87b7f6c214d3aab95134
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-13 12:17:40 +0200

    Adaptation du script d'archivage

--- bin/archiveTemps.sh
+++ bin/archiveTemps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATE=$(cat temps.csv | grep -v "^Date;Projet;Description;" | sort | tail -n 1 | cut -d ";" -f 1,1 | sed 's/-//g')
+DATE=$(cat temps.csv | grep -v "^Date;Client;Description;" | sort | tail -n 1 | cut -d ";" -f 1,1 | sed 's/-//g')
 
 if ! test "$DATE"; then
     echo "Rien à facturer"
@@ -15,14 +15,14 @@ cp temps.csv temps/$DATE/temps.csv
 
 cat temps/$DATE/temps.csv | grep -v "^Date;Projet;Description;" | cut -d ";" -f 2 | sort | uniq | while read projet  
 do
-    echo "Date;Projet;Description;Temps (en heure);Tache" > temps/$DATE/"$projet".csv
+    echo "Date;Client;Description;Temps (en heure);Catégorie" > temps/$DATE/"$projet".csv
     cat temps/$DATE/temps.csv | sort | grep ";$projet;" >> temps/$DATE/"$projet".csv
     echo "Création de temps/$DATE/"$projet".csv"
 done
 
 echo "Ajout de temps.csv dans temps_historique.csv"
 
-cat temps/$DATE/temps.csv | grep -v "^Date;Projet;Description;" >> temps_historique.csv
+cat temps/$DATE/temps.csv | grep -v "^Date;Client;Description;" >> temps_historique.csv
 
 sort -r temps_historique.csv > temps_historique.csv.tmp
 mv temps_historique.csv{.tmp,}
@@ -30,4 +30,4 @@ mv temps_historique.csv{.tmp,}
 bash bin/recap.sh > temps/$DATE/recap.csv
 
 echo "Réinialisation du fichier temps.csv"
-echo "Date;Projet;Description;Temps (en heure);Tache (pas obligatoire)" > temps.csv
+echo "Date;Client;Description;Temps (en heure);Catégorie" > temps.csv
