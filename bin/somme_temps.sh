225f33a0c3bfa416779c335929e771ab21cc922b
Tangui Morlier <tangui@tangui.eu.org>
2026-03-02 11:17:12 +0100

    Plus de détails dans la somme des temps

--- bin/somme_temps.sh
+++ bin/somme_temps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-csv=$1
+csv=$*
 
-echo -n "Temps en jours : "
-echo "( " $(cat $csv  | awk -F ';' '{print $4 " + "}' | grep -E '^[0-9]' | sed 's/,/./' ) " 0 ) / 7 " | bc -l
-
+echo "Temps en jours : "
+echo
+cat $csv | grep -v '^Date;' | grep ';' | sed 's/,/./g' | awk -F ';' 'BEGIN{total=0}{	total = total + $4;	clients[$2] = 0.0 + clients[$2] + $4} END{ for (c in clients) { printf("    %-15s %5.2f j (%6.2f h) %5.1f j.fact\n", c, clients[c] / 7, clients[c], int(clients[c] / 3.5) / 2); facturable = facturable + int(clients[c] / 3.5) / 2; } printf("\n    %-15s %5.2f j            %5.1f\n", "TOTAL", total / 7, facturable);}'
+#echo "( " $(cat $csv  | awk -F ';' '{print $4 " + "}' | grep -E '^[0-9]' | sed 's/,/./' ) " 0 ) / 7 " | bc -l
